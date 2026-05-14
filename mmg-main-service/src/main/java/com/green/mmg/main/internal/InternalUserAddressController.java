a83155f4921f135f2179e788029a0537845442de
woojeong <dayontheride@gmail.com>
2026-05-14 10:41:51 +0900

    feat: 관리자 회원관리 기본 주소 조회 API

--- mmg-main-service/src/main/java/com/green/mmg/main/internal/InternalUserAddressController.java
+++ mmg-main-service/src/main/java/com/green/mmg/main/internal/InternalUserAddressController.java
@@ -3,6 +3,7 @@ package com.green.mmg.main.internal;
 import com.green.mmg.common.dto.ResultResponse;
 import com.green.mmg.main.address.UserAddressRepository;
 import com.green.mmg.main.address.model.UserAddressRes;
+import com.green.mmg.main.internal.dto.UserDefaultAddressRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,17 @@ public class InternalUserAddressController {
     @GetMapping("/{userNo}/address")
     public ResultResponse<List<UserAddressRes>> getUserAddresses(@PathVariable long userNo) {
         return new ResultResponse<>("주소 조회 완료", userAddressRepository.findAllByUserNo(userNo));
+    }
+
+    @Transactional(readOnly = true)
+    @GetMapping("/addresses/default")
+    public ResultResponse<List<UserDefaultAddressRes>> getDefaultAddresses(@RequestParam List<Long> userNos) {
+
+        if(userNos == null || userNos.isEmpty()) {
+            return new ResultResponse<>("주소 목록 조회", List.of());
+        }
+
+        return new ResultResponse<>("주소 목록 조회", userAddressRepository.findDefaultAddressesByUserNos(userNos));
     }
 
 }
