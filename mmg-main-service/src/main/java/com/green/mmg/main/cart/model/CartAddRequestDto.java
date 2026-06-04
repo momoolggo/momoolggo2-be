3bfc8283422693eb94c10e1aacd7967c4f4d0703
woojeong <dayontheride@gmail.com>
2026-06-04 11:03:59 +0900

    fix: 운영 배포 라우팅 및 옵션 변경사항 반영

--- mmg-main-service/src/main/java/com/green/mmg/main/cart/model/CartAddRequestDto.java
+++ mmg-main-service/src/main/java/com/green/mmg/main/cart/model/CartAddRequestDto.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 
@@ -12,4 +14,12 @@ public class CartAddRequestDto {
     private Long userNo;
     private Long menuId;
     private int quantity;
+    private List<CartOptionRequestDto> selectedOptions;
+
+    @Getter
+    @Setter
+    public static class CartOptionRequestDto {
+        private Long optionCategoryNo;
+        private Long optionId;
+    }
 }
